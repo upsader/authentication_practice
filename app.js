77b88a51af01375b69433d39ec4cc4a4d04f1571
Olegs Kuzicevs <61397803+upsader@users.noreply.github.com>
2020-06-09 01:26:17 +0300

    Add submit to secret.ejs

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ const userSchema = new mongoose.Schema ({
   email: String,
   password: String,
   googleId: String,
-  facebookId: String
+  facebookId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -124,11 +125,39 @@ app.get("/logout", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
+  User.find({'secret': {$ne: null}}, function(err, foundUsers){
+    if (err){
+      console.log(err);
+    } else {
+      if (foundUsers){
+        res.render('secrets', {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+
+app.get("/submit", function(req, res){
   if (req.isAuthenticated()){
-    res.render('secrets');
+    res.render('submit');
   } else{
     res.redirect('/login');
   }
+});
+
+app.post('/submit', function(req, res){
+  const submittedSecret = req.body.secret;
+  User.findById(req.user.id, function(err, foundUser){
+    if (err){
+      console.log(err);
+    } else{
+      if (foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect('/secrets');
+        });
+      }
+    }
+  });
 });
 
 app.post("/register", function(req, res){
